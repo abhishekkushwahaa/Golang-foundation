a223fee9f73431b417c32a8cffc083aeba92a9fa
Abhishek Kushwaha <92770059+abhishekkushwahaa@users.noreply.github.com>
2024-03-14 20:48:21 +0530

    update a course

--- 19buildapi/main.go
+++ 19buildapi/main.go
@@ -100,3 +100,29 @@ func createCourse(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(course)
 	return
 }
+
+// update a course
+func updateCourse(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Update a course")
+	w.Header().Set("Content-Type", "application/json")
+
+	// get the course id from the url
+	params := mux.Vars(r);
+
+	// loop through the courses to find, add and remove the course with the id
+
+	for index, course := range courses {
+		if course.CourseID == params["id"]{
+			courses = append(courses[:index], courses[index+1:]...)
+
+			var course Course
+			_ = json.NewDecoder(r.Body).Decode(&course)
+
+			course.CourseID = params["id"]
+			courses = append(courses, course)
+			json.NewEncoder(w).Encode(course)
+			return
+		}
+	}
+
+}
