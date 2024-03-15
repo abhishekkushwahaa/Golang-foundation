4d23b8dc2af14e95eb79e853560b5bcad1d86a40
Abhishek Kushwaha <92770059+abhishekkushwahaa@users.noreply.github.com>
2024-03-15 21:34:35 +0530

    define models

---
+++ 20mongoapi/model/models.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type Netflix struct {
+	ID primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
+	Movie string `json:"movie,omitempty" bson:"movie,omitempty"`
+	Watched bool `json:"watched,omitempty" bson:"watched,omitempty"`
+}
