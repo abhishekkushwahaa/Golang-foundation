95e7f90d3dcbac6269709b1defa57a680c5e31c3
Abhishek Kushwaha <92770059+abhishekkushwahaa@users.noreply.github.com>
2024-03-18 18:58:06 +0530

    waitgroup

--- 21goroutines/main.go
+++ 21goroutines/main.go
@@ -2,20 +2,46 @@ package main
 
 import (
 	"fmt"
-	"time"
+	"net/http"
+	"sync"
 )
 
-func main() {
-	// goroutine means a function that runs concurrently with other functions
-	go greeter("Hello G")
+var wg sync.WaitGroup
 
-	greeter("Sir")
+func main() {
+	// // goroutine means a function that runs concurrently with other functions
+	// go greeter("Hello G")
+
+	// greeter("Sir")
+
+	websiteList := []string{
+		"https://golang.org",
+		"https://go.dev",
+		"https://lco.dev",
+		"https://fb.com",
+	}
+
+	for _, web := range websiteList {
+		go getStatusCode(web)
+		wg.Add(1)
+	}
+	wg.Wait()
+	fmt.Println("All go routines are done")
 }
 
-func greeter(s string) {
-	for i := 0; i < 5; i++ {
-		// It is not better approach please use sync package for better approach
-		time.Sleep(3 * time.Millisecond)
-		fmt.Println(s)
+// func greeter(s string) {
+// 	for i := 0; i < 5; i++ {
+// 		// It is not better approach please use sync package for better approach
+// 		time.Sleep(3 * time.Millisecond)
+// 		fmt.Println(s)
+// 	}
+// }
+
+func getStatusCode(endpoint string)  {
+	defer wg.Done()
+	res, err := http.Get(endpoint)
+	if err != nil {
+		fmt.Println("OOPs in endpoint")
 	}
+	fmt.Println("Status code is", res.StatusCode)
 }
